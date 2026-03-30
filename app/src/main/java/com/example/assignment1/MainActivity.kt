a3168d0032745a579bd9e48649faf73b0faba6e8
Makhadzi-Spyder <mmbobvu@gmail.com>
2026-03-30 21:00:00 +0200

    Added components and declared variables
    Added components to the UI and declared said components in the code

---
+++ app/src/main/java/com/example/assignment1/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.assignment1
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        //retrieving all relevant components added to UI
+        val btnReset = findViewById<Button>(R.id.btnReset)
+        val edtTime = findViewById<EditText>(R.id.edtTime)
+        val txtSpark = findViewById<TextView>(R.id.txtSpark)
+        val btnSpark = findViewById<Button>(R.id.btnSpark)
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
